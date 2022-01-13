5bbd6af4f120d03aff45a7137c118794796a3a0f
ali-abdelqawy <ali.abdelqawy@outlook.com>
2022-01-13 04:59:38 +0200

    remove Add Custom Ingredient from ingredients array

--- src/constants/ingredients.js
+++ src/constants/ingredients.js
@@ -2,21 +2,19 @@ export const ingredients = [
   {
     id: 1,
     name: "Espresso",
+    caffeinePerShot: 20,
     icon: "https://cdn-icons-png.flaticon.com/512/2302/2302393.png",
   },
   {
     id: 2,
     name: "Water",
+    caffeinePerShot: 0,
     icon: "https://cdn-icons-png.flaticon.com/512/684/684609.png",
   },
   {
     id: 3,
     name: "Milk",
+    caffeinePerShot: 0,
     icon: "https://cdn-icons-png.flaticon.com/512/684/684614.png",
-  },
-  {
-    id: 4,
-    name: "Add",
-    icon: "https://cdn-icons-png.flaticon.com/512/992/992651.png",
   },
 ];
