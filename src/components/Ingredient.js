fc701ab4306125f4813f517d24b0167b6bfd6963
ali-abdelqawy <ali.abdelqawy@outlook.com>
2022-01-13 05:01:12 +0200

    add caffeiene per shot

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { IngredientCounter } from "./IngredientCounter";
 
-export const Ingredient = ({ name, icon }) => {
+export const Ingredient = ({
+  name,
+  icon,
+  caffeinePerShot,
+  setTotalCaffeine,
+}) => {
   return (
     <div className="ingredient">
       <img src={icon} className="ingredientIcon" />
       <h4 className="ingredientName">{name}</h4>
-
-      {name !== "Add" && <IngredientCounter />}
+      <p>
+        <strong>Caffiene per shot:</strong> {caffeinePerShot} MG
+      </p>
+      <IngredientCounter
+        setTotalCaffeine={setTotalCaffeine}
+        caffeinePerShot={caffeinePerShot}
+      />
     </div>
   );
 };
